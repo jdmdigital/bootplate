cc4956c91e759610df2bee24107700cd4267e00a
JDM Digital <nerds@jdmdigital.co>
2016-03-15 12:22:58 -0500

    Starting #6
    Added one_half shortcodes to start how the workflow might work.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -19,6 +19,39 @@ if(!function_exists('jdm_antispam_shortcode')) {
 	add_shortcode( 'antispam', 'jdm_antispam_shortcode' );
 }
 
+// [one_half xclass="col-md-6" openrow='y'] [/one_half] - Includes opening row
+if(!function_exists('one_half_shortcode')) {
+	function one_half_shortcode( $atts, $content = null ) {
+		extract( shortcode_atts( array(
+			'xclass' => 'col-md-6',
+			'openrow' => 'y',
+		), $atts ) );
+		$html = '';
+		if(strtolower($openrow) == 'y' || strtolower($openrow) == 'yes') {
+			$html .= '<div class="row">';
+		}
+		$html .= '	<div class="'.$xclass.'">'.do_shortcode($content).'</div>';
+	}
+	add_shortcode( 'one_half', 'one_half_shortcode' );
+}
+
+// [one_half_last xclass="col-md-6" openrow='y'] [/one_half_last] - closes opening row
+if(!function_exists('one_half_last_shortcode')) {
+	function one_half_last_shortcode( $atts, $content = null ) {
+		extract( shortcode_atts( array(
+			'xclass' => 'col-md-6',
+			'openrow' => 'y',
+		), $atts ) );
+		$html = '';
+		
+		$html .= '	<div class="'.$xclass.'">'.do_shortcode($content).'</div>';
+		if(strtolower($openrow) == 'y' || strtolower($openrow) == 'yes') {
+			$html .= '</div><!--/row one_half_last-->';
+		}
+	}
+	add_shortcode( 'one_half_last', 'one_half_last_shortcode' );
+}
+
 // [alert type="warning" dismiss="n"] [/alert]
 if(!function_exists('alert_shortcode')) {
 	function alert_shortcode( $atts, $content = null ) {
