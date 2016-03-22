d5f128d646868a4a62c343cfa56b3e228ba13fb4
JDM Digital <nerds@jdmdigital.co>
2016-03-22 13:23:59 -0500

    Fix #25
    Default Nav template created and get_template_part() function added to
    header.php.

--- header.php
+++ header.php
@@ -24,36 +24,5 @@
 
 <body <?php body_class(); ?>>
 
-<nav class="navbar navbar-full navbar-dark bg-inverse navbar-inverse hamburger-cross">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggler hidden-sm-up is-closed navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu" aria-expanded="false" aria-controls="primary-menu">
-				<span class="sr-only screen-reader-text">Toggle Menu</span>
-				<span class="hamb-top"></span>
-				<span class="hamb-middle"></span>
-				<span class="hamb-bottom"></span>
-			</button>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand" rel="home"><?php bloginfo( 'name' ); ?></a>
-		</div>
-		<div id="primary-menu" class="collapse navbar-collapse pull-sm-right navbar-toggleable-xs">
-			<ul class="nav navbar-nav navbar-right">
-				<?php
-				wp_nav_menu( 
-					array(
-						'menu'              => 'primary',
-						'theme_location'    => 'primary',
-						'depth'             => 2,
-						'container'         => false,
-						'menu_class'      => false,
-						'items_wrap'      => '%3$s',
-						//'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-						'walker'            => new wp_bootstrap_navwalker()
-					)
-				);
-				?>
-				<li><a href="/search/"><span class="bp-search"></span><span class="hidden-sm-up"><?php _e('Site Search', 'bootplate'); ?></span></a></li>
-			</ul>
-		</div><!-- /#primary-menu -->
-	</div><!--/.container-->
-</nav>
+<?php get_template_part('nav'); ?>
 <a class="skip-link sr-only screen-reader-text" href="#content"><?php _e('Skip to content', 'bootplate'); ?></a>
