8b4e022bf0eda3c57f1a3b0b6642a1ee17024f72
JDM Digital <nerds@jdmdigital.co>
2016-08-02 14:41:58 -0500

    Add More Header Options

---
+++ page-thirdheight.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Template Name: Page: 1/3rd Height
+*/
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+<header class="<?php echo header_classes(); ?>"<?php the_header_bgimg(get_the_id(), 'full'); ?>>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<?php the_title( '<h1 class="display-3">', '</h1>' ); ?>
+				<?php if(have_bootplate_subtitle()) { bootplate_subtitle(); } ?>
+				
+				<?php 
+				if(have_bootplate_btns()) : 
+					if(have_cta1() && have_cta2()) :
+				?>
+				<div class="row margin-top">
+					<div class="col-sm-6">
+						<?php the_cta(1); ?>
+					</div>
+					<div class="col-sm-6">
+						<?php the_cta(2); ?>
+					</div>
+				</div>
+				
+				<?php 
+					elseif(have_cta1() && !have_cta2()) :
+				?>
+				<div class="row margin-top">
+					<div class="col-sm-12">
+						<?php the_cta(1); ?>
+					</div>
+				</div>
+				
+				<?php
+					endif; // end which buttons?
+				endif; // end have_btns
+				?>
+			</div>
+		</div><!--/row-->
+	</div><!--/.container-->
+</header>
+
+<section id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="container">
+		<?php the_content(); ?>
+	</div>
+</section>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
